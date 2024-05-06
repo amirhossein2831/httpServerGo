cee6c0db2596de456477f2e6910cfcd14d9e83ca
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-06 10:11:25 -0700

    :hammer: chore: create the server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"httpServer/Http"
+	"log"
+	"net/http"
+)
+
+func main() {
+	Http.Routing()
+
+	println("server started at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
