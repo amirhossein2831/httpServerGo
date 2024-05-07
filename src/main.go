1cabebece3c9057ed1c99ef02e755c30c24e8df6
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-07 06:38:59 -0700

    :hammer: chore: seed User

--- src/main.go
+++ src/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/amirhossein2831/httpServerGo/src/Http"
+	"github.com/amirhossein2831/httpServerGo/src/Model"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// seed data
+	Model.SeedUser()
+
+	// instantiate router
 	router := Http.Routing()
 
+	// run server
 	println("server started at port 8080")
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
