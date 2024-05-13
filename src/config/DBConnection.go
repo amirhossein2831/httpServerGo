3dbc4eb9c4aaf25301aaa49c7efcb8968f9e9bb6
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-13 09:40:31 -0700

    :card_file_box: DB: connect to DB

---
+++ src/config/DBConnection.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, name)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
