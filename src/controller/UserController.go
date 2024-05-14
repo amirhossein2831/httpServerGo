ebbf65b8bb37cefdb8af617a861bde2da04949dd
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-14 09:33:20 -0700

    :truck: chore: change name of http to util

--- src/controller/UserController.go
+++ src/controller/UserController.go
@@ -1,8 +1,8 @@
 package controller
 
 import (
-	http2 "github.com/amirhossein2831/httpServerGo/src/http"
 	"github.com/amirhossein2831/httpServerGo/src/repositories"
+	"github.com/amirhossein2831/httpServerGo/src/util"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,46 +14,46 @@ type UserController struct {
 func (c *UserController) Index(w http.ResponseWriter, r *http.Request) {
 	users, err := repositories.GetUsers()
 	if err != nil {
-		http2.JsonError(w, err)
+		util.JsonError(w, err)
 		return
 	}
 
-	http2.JsonResponse(w, http.StatusOK, users)
+	util.JsonResponse(w, http.StatusOK, users)
 }
 
 func (c *UserController) Show(w http.ResponseWriter, r *http.Request) {
 	user, err := repositories.GetUser(mux.Vars(r)["id"])
 	if err != nil {
-		http2.JsonError(w, err)
+		util.JsonError(w, err)
 		return
 	}
 
-	http2.JsonResponse(w, http.StatusOK, user)
+	util.JsonResponse(w, http.StatusOK, user)
 }
 
 func (c *UserController) Create(w http.ResponseWriter, r *http.Request) {
 	user, err := repositories.CreateUser(r)
 	if err != nil {
-		http2.JsonError(w, err)
+		util.JsonError(w, err)
 		return
 	}
-	http2.JsonResponse(w, http.StatusCreated, user)
+	util.JsonResponse(w, http.StatusCreated, user)
 }
 
 func (c *UserController) Update(w http.ResponseWriter, r *http.Request) {
 	user, err := repositories.UpdateUser(r, mux.Vars(r)["id"])
 	if err != nil {
-		http2.JsonError(w, err)
+		util.JsonError(w, err)
 		return
 	}
-	http2.JsonResponse(w, http.StatusOK, user)
+	util.JsonResponse(w, http.StatusOK, user)
 }
 
 func (c *UserController) Delete(w http.ResponseWriter, r *http.Request) {
 	err := repositories.DeleteUser(mux.Vars(r)["id"])
 	if err != nil {
-		http2.JsonError(w, err)
+		util.JsonError(w, err)
 		return
 	}
-	http2.JsonResponse(w, http.StatusOK, struct{}{})
+	util.JsonResponse(w, http.StatusOK, struct{}{})
 }
