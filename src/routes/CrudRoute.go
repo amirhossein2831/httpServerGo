2e0eb76af40db7c95af40c851fa8ff28fe96c5d2
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-14 08:58:45 -0700

    :hammer: chore: create a  CrudRoute which create all http method for given controller

---
+++ src/routes/CrudRoute.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"fmt"
+	"github.com/amirhossein2831/httpServerGo/src/controller"
+	"github.com/gorilla/mux"
+)
+
+func CrudRoute(r *mux.Router, path string, c controller.Crud) {
+	r.HandleFunc(fmt.Sprintf("/%v/", path), c.Index).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%v/", path), c.Create).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/%v/{id}", path), c.Update).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%v/{id}", path), c.Update).Methods("PUT")
+	r.HandleFunc(fmt.Sprintf("/%v/{id}", path), c.Delete).Methods("DELETE")
+}
