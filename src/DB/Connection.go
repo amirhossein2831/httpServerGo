95ca681d49c5419ce8fa5161402cca73f97a3aae
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-19 13:30:11 -0700

    :hammer: chore: get single tone instance of the DB

--- src/DB/Connection.go
+++ src/DB/Connection.go
@@ -2,57 +2,42 @@ package DB
 
 import (
 	"errors"
-	"fmt"
+	"github.com/amirhossein2831/httpServerGo/src/DB/msq"
+	"github.com/amirhossein2831/httpServerGo/src/DB/pgs"
 	"github.com/amirhossein2831/httpServerGo/src/config"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"sync"
 )
 
-func ConnectDB() (*gorm.DB, error) {
-	dbType := config.GetInstance().Get("DB_TYPE")
+var (
+	dbInstance Database
+	once       sync.Once
+)
 
-	if dbType == "postgres" {
-		return postgresConnection()
-	} else if dbType == "mysql" {
-		return mysqlConnection()
-	}
-
-	return nil, errors.New("the db type is not supported")
+type Database interface {
+	Connect() (*gorm.DB, error)
+	GetDb() *gorm.DB
+	SetDb(db *gorm.DB)
 }
 
-func mysqlConnection() (*gorm.DB, error) {
-	dsn := fmt.Sprintf(
-		"%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
-		config.GetInstance().Get("MYSQL_USER"),
-		config.GetInstance().Get("MYSQL_PASS"),
-		config.GetInstance().Get("DB_HOST"),
-		config.GetInstance().Get("MYSQL_PORT"),
-		config.GetInstance().Get("DB_NAME"),
-	)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func postgresConnection() (*gorm.DB, error) {
-	dsn := fmt.Sprintf(
-		"host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Shanghai",
-		config.GetInstance().Get("DB_HOST"),
-		config.GetInstance().Get("PG_USER"),
-		config.GetInstance().Get("PG_PASS"),
-		config.GetInstance().Get("DB_NAME"),
-		config.GetInstance().Get("PG_PORT"),
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+func GetInstance() Database {
+	once.Do(func() {
+		dbType := config.GetInstance().Get("DB_TYPE")
+		switch dbType {
+		case "postgres":
+			dbInstance = &pgs.PostgresDB{}
+		case "mysql":
+			dbInstance = &msq.MysqlDB{}
+		default:
+			log.Fatal(errors.New("unsupported db type"))
+		}
+		db, err := dbInstance.Connect()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbInstance.SetDb(db)
+		println("connected to DB successfully")
+	})
+	return dbInstance
 }
