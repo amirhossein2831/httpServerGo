3f62196af942974d1347faf7021ff7101237acab
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-19 04:47:55 -0700

    :hammer: chore: check the validation of the token

--- src/Auth/JWT.go
+++ src/Auth/JWT.go
@@ -1,13 +1,14 @@
 package Auth
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
 
 var secretKey = []byte("secret-key")
 
-func createToken(email string) (string, error) {
+func CreateToken(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
 		jwt.MapClaims{
 			"email": email,
@@ -19,4 +20,19 @@ func createToken(email string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return errors.New("invalid token")
+	}
+
+	return nil
 }
