94cab22de872bf3aca3a877e2de7e9e1479b3426
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-19 04:47:00 -0700

    :hammer: chore: implement a method to createJWTToken

--- src/Auth/JWT.go
+++ src/Auth/JWT.go
@@ -1,1 +1,22 @@
 package Auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var secretKey = []byte("secret-key")
+
+func createToken(email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"email": email,
+			"exp":   time.Now().Add(time.Hour * 48).Unix(),
+		})
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
