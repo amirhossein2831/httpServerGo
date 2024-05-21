2696906f774a1372b3c4842bb20629ac867420f7
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-21 10:42:14 -0700

    :hammer: chore: update bookContrller

--- src/http/controller/BookController.go
+++ src/http/controller/BookController.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"encoding/json"
+	"github.com/amirhossein2831/httpServerGo/src/http/request"
 	"github.com/amirhossein2831/httpServerGo/src/repositories"
 	"github.com/amirhossein2831/httpServerGo/src/util"
 	"github.com/gorilla/mux"
@@ -32,7 +34,19 @@ func (c *BookController) Show(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
-	book, err := repositories.CreateBook(r)
+	var bookRequest request.BookRequest
+	err := json.NewDecoder(r.Body).Decode(&bookRequest)
+	if err != nil {
+		util.JsonError(w, err)
+		return
+	}
+
+	book, err := bookRequest.Validate()
+	if err != nil {
+		util.JsonError(w, err)
+		return
+	}
+	book, err = repositories.CreateBook(book)
 	if err != nil {
 		util.JsonError(w, err)
 		return
@@ -41,7 +55,20 @@ func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *BookController) Update(w http.ResponseWriter, r *http.Request) {
-	book, err := repositories.UpdateBook(r, mux.Vars(r)["id"])
+	var bookRequest request.BookRequest
+	err := json.NewDecoder(r.Body).Decode(&bookRequest)
+	if err != nil {
+		util.JsonError(w, err)
+		return
+	}
+
+	book, err := bookRequest.Validate()
+	if err != nil {
+		util.JsonError(w, err)
+		return
+	}
+
+	book, err = repositories.UpdateBook(book, mux.Vars(r)["id"])
 	if err != nil {
 		util.JsonError(w, err)
 		return
@@ -50,7 +77,7 @@ func (c *BookController) Update(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *BookController) Delete(w http.ResponseWriter, r *http.Request) {
-	err := repositories.DeleteBook(mux.Vars(r)["id"])
+	err := repositories.SoftDeleteBook(mux.Vars(r)["id"])
 	if err != nil {
 		util.JsonError(w, err)
 		return
