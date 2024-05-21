8576ec62af9f91cf24e0356e3ca5ddf96d87b827
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-21 02:15:36 -0700

    :hammer: chore: update RoleMiddle ware

--- src/Middleware/Role.go
+++ src/Middleware/Role.go
@@ -8,10 +8,25 @@ import (
 	"strings"
 )
 
-func RoleMiddleware(role string) func(http.Handler) http.Handler {
+func RoleMiddleware(roles []string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			hasRole := checkRole(r, role)
+			var user model.User
+
+			authHeader := r.Header.Get("Authorization")
+			tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+
+			claims, err := Auth.RetainClaim(tokenString)
+			if err != nil {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+			}
+
+			err = DB.GetInstance().GetDb().Where("email = ?", claims["email"]).Preload("Roles").Preload("Roles.Permissions").First(&user).Error
+			if err != nil {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+			}
+
+			hasRole := model.HasRole(user, roles)
 			if hasRole {
 				next.ServeHTTP(w, r)
 			} else {
@@ -19,30 +34,4 @@ func RoleMiddleware(role string) func(http.Handler) http.Handler {
 			}
 		})
 	}
-}
-
-func checkRole(r *http.Request, role string) bool {
-	var user model.User
-
-	authHeader := r.Header.Get("Authorization")
-	tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
-
-	claims, err := Auth.RetainClaim(tokenString)
-	if err != nil {
-		return false
-
-	}
-
-	err = DB.GetInstance().GetDb().Where("email = ?", claims["email"]).Preload("Roles").Preload("Roles.Permissions").First(&user).Error
-	if err != nil {
-		return false
-	}
-
-	for _, sr := range user.Roles {
-		if sr.Name == role {
-			return true
-		}
-	}
-
-	return false
 }
