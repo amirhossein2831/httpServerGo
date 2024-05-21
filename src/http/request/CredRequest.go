2b63d6716a145aff34f0583207560a0906381e04
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-21 10:29:16 -0700

    :hammer: chore: update CredRequest

--- src/http/request/CredRequest.go
+++ src/http/request/CredRequest.go
@@ -1,6 +1,20 @@
 package request
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type CredRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `validate:"required,email" json:"email" `
+	Password string `validate:"required,min=8" json:"password"`
+}
+
+func (c *CredRequest) Validate() (CredRequest, error) {
+	validate := validator.New()
+	err := validate.Struct(c)
+
+	if err != nil {
+		return CredRequest{}, validationError(err)
+	}
+	return *c, nil
 }
