ed28c7f1cf76cc77b1228cc263ef762c799e5aed
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-23 15:53:55 -0700

    :hammer: chore: update UserController

--- src/http/controller/UserController.go
+++ src/http/controller/UserController.go
@@ -2,9 +2,9 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/amirhossein2831/httpServerGo/src/http/Response"
 	"github.com/amirhossein2831/httpServerGo/src/http/request"
 	"github.com/amirhossein2831/httpServerGo/src/http/service"
-	"github.com/amirhossein2831/httpServerGo/src/util"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -22,60 +22,95 @@ func NewUserController() *UserController {
 func (c *UserController) Index(w http.ResponseWriter, r *http.Request) {
 	users, err := c.service.Index()
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
-	util.JsonResponse(w, http.StatusOK, users)
+
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(users).
+		Send(w)
 }
 
 func (c *UserController) Show(w http.ResponseWriter, r *http.Request) {
 	user, err := c.service.Show(mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusOK, user)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(user).
+		Send(w)
 }
 
 func (c *UserController) Create(w http.ResponseWriter, r *http.Request) {
 	var userRequest request.UserRequest
 	err := json.NewDecoder(r.Body).Decode(&userRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 	user, err := c.service.Create(&userRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusCreated, user)
+	Response.NewJson().
+		SetStatusCode(http.StatusCreated).
+		SetSuccess(true).
+		SetData(user).
+		Send(w)
 }
 
 func (c *UserController) Update(w http.ResponseWriter, r *http.Request) {
 	var userRequest request.UserRequest
 	err := json.NewDecoder(r.Body).Decode(&userRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
 	user, err := c.service.Update(&userRequest, mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusOK, user)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(user).
+		Send(w)
 }
 
 func (c *UserController) Delete(w http.ResponseWriter, r *http.Request) {
 	err := c.service.Delete(mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
-	util.JsonResponse(w, http.StatusOK, struct{}{})
+	Response.NewJson().
+		SetStatusCode(http.StatusOK). // i don't know why but panic on 204 (no content)
+		SetSuccess(true).
+		SetData(struct{}{}).
+		Send(w)
 }
