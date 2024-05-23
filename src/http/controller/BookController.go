19a778062585ee4cb6d26f62412f24bf7f4b14f9
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-23 15:56:10 -0700

    :hammer: chore: update BookController

--- src/http/controller/BookController.go
+++ src/http/controller/BookController.go
@@ -2,9 +2,9 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/amirhossein2831/httpServerGo/src/http/Response"
 	"github.com/amirhossein2831/httpServerGo/src/http/request"
 	"github.com/amirhossein2831/httpServerGo/src/http/service"
-	"github.com/amirhossein2831/httpServerGo/src/util"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -23,62 +23,96 @@ func NewBookController() *BookController {
 func (c *BookController) Index(w http.ResponseWriter, r *http.Request) {
 	books, err := c.service.Index()
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusOK, books)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(books).
+		Send(w)
 }
 
 func (c *BookController) Show(w http.ResponseWriter, r *http.Request) {
 	book, err := c.service.Show(mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusOK, book)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(book).
+		Send(w)
 }
 
 func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
 	var bookRequest request.BookRequest
 	err := json.NewDecoder(r.Body).Decode(&bookRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
 	book, err := c.service.Create(&bookRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusCreated, book)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(book).
+		Send(w)
 }
 
 func (c *BookController) Update(w http.ResponseWriter, r *http.Request) {
 	var bookRequest request.BookRequest
 	err := json.NewDecoder(r.Body).Decode(&bookRequest)
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
 	book, err := c.service.Update(&bookRequest, mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
 
-	util.JsonResponse(w, http.StatusOK, book)
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(book).
+		Send(w)
 }
 
 func (c *BookController) Delete(w http.ResponseWriter, r *http.Request) {
 	err := c.service.Delete(mux.Vars(r)["id"])
 	if err != nil {
-		util.JsonError(w, err)
+		Response.NewJson().
+			SetData(err).
+			Send(w)
 		return
 	}
-	util.JsonResponse(w, http.StatusOK, struct{}{})
+	Response.NewJson().
+		SetStatusCode(http.StatusOK).
+		SetSuccess(true).
+		SetData(struct{}{}).
+		Send(w)
 }
