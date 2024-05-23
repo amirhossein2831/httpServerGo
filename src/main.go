33d8819bd1757675bc100e10755acab6c78129c1
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-23 08:34:10 -0700

    :hammer: chore: use log module in main

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/amirhossein2831/httpServerGo/src/App"
 	"github.com/amirhossein2831/httpServerGo/src/DB"
+	"github.com/amirhossein2831/httpServerGo/src/Logger"
 	"github.com/amirhossein2831/httpServerGo/src/config"
 	"github.com/amirhossein2831/httpServerGo/src/model"
 	"github.com/amirhossein2831/httpServerGo/src/routes"
@@ -17,23 +18,21 @@ func main() {
 	// init the app
 	App.Configure()
 
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+	defer Logger.GetInstance().GetLogger().Sync()
 
-	// migrate Tables
 	err := model.Migrate(DB.GetInstance().GetDb())
 	if err != nil {
-		logger.Error("Database migration failed", zap.Error(err))
+		Logger.GetInstance().GetLogger().Error("Database migration failed", zap.Error(err))
 		return
 	}
-	logger.Info("Table Migrate successfully",
+	Logger.GetInstance().GetLogger().Info("Table Migrate successfully",
 		zap.Time("timestamp", time.Now()),
 	)
 
-	println("Table Migrate successfully")
-
-	// run server
-	println(fmt.Sprintf("app started at port %v", config.GetInstance().Get("APP_PORT")))
+	Logger.GetInstance().GetLogger().Info("App started successfully",
+		zap.String("Port", config.GetInstance().Get("APP_PORT")),
+		zap.Time("timestamp", time.Now()),
+	)
 	if err := http.ListenAndServe(fmt.Sprintf(":%v", config.GetInstance().Get("APP_PORT")),
 		routes.GetInstance().GetRouter()); err != nil {
 		log.Fatal(err)
