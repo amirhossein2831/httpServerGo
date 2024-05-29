8d6dca9f049341c1a16f213d6aef5b62831422fd
amir <amirmemool12@gmail.com>
2024-05-29 19:48:00 +0330

    :hammer: chore: update main

--- src/main.go
+++ src/main.go
@@ -9,32 +9,32 @@ import (
 	"github.com/amirhossein2831/httpServerGo/src/model"
 	"github.com/amirhossein2831/httpServerGo/src/routes"
 	"go.uber.org/zap"
-	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	// init the app
+	// Init the app
 	App.Configure()
-
 	defer Logger.GetInstance().GetLogger().Sync()
 
+	// Migrate Tables
 	err := model.Migrate(DB.GetInstance().GetDb())
 	if err != nil {
-		Logger.GetInstance().GetLogger().Error("Database migration failed", zap.Error(err))
+		Logger.GetInstance().GetLogger().Error("Database migration failed", zap.Error(err), zap.Time("timestamp", time.Now()))
 		return
 	}
 	Logger.GetInstance().GetLogger().Info("Table Migrate successfully",
 		zap.Time("timestamp", time.Now()),
 	)
 
+	// Start the App and server
 	Logger.GetInstance().GetLogger().Info("App started successfully",
 		zap.String("Port", config.GetInstance().Get("APP_PORT")),
 		zap.Time("timestamp", time.Now()),
 	)
 	if err := http.ListenAndServe(fmt.Sprintf(":%v", config.GetInstance().Get("APP_PORT")),
 		routes.GetInstance().GetRouter()); err != nil {
-		log.Fatal(err)
+		Logger.GetInstance().GetLogger().Error("Failed to start the server", zap.Error(err), zap.Time("timestamp", time.Now()))
 	}
 }
