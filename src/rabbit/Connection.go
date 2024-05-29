981c507abca5e9e70a6dc39e83bb20bb4bd5d051
amir <amirmemool12@gmail.com>
2024-05-29 19:44:40 +0330

    :hammer: chore: create rabbit module

---
+++ src/rabbit/Connection.go
@@ -0,0 +1,49 @@
+package rabbit
+
+import (
+	"github.com/amirhossein2831/httpServerGo/src/Logger"
+	"github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"go.uber.org/zap"
+	"log"
+	"time"
+
+	"sync"
+)
+
+var (
+	conInstance Connection
+	once        sync.Once
+)
+
+type Connection interface {
+	GetConnection() *amqp.Connection
+	SetConnection(conn *amqp.Connection)
+}
+
+type RbConnection struct {
+	conn *amqp091.Connection
+}
+
+func GetInstance() Connection {
+	once.Do(func() {
+		conn, err := amqp.Dial("amqp://root:password@localhost:5672/")
+		if err != nil {
+			Logger.GetInstance().GetLogger().Error("Failed to connect to RabbitMQ", zap.Error(err), zap.Time("timestamp", time.Now()))
+			log.Fatal(err)
+		}
+		Logger.GetInstance().GetLogger().Info("Connect to RabbitMQ Successfully", zap.Time("timestamp", time.Now()))
+		conInstance = &RbConnection{
+			conn: conn,
+		}
+	})
+	return conInstance
+}
+
+func (c *RbConnection) GetConnection() *amqp.Connection {
+	return c.conn
+}
+
+func (c *RbConnection) SetConnection(conn *amqp.Connection) {
+	c.conn = conn
+}
